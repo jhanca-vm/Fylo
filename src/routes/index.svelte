26afab5c17755f01fe4bc248ac73fa501557f4d6
Jhanca <jhanviloria45@gmail.com>
2020-05-13 00:18:33 -0500

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Fylo</title>
+</svelte:head>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Open+Sans&family=Raleway:wght@400;700&display=swap');
+	
+	:global(body) {
+		margin: 0;
+		font-family: 'Open Sans', sans-serif;
+		color: hsl(238, 22%, 44%);
+	}
+	
+	:global(h1, h2, h3) {
+		font-family: 'Raleway', sans-serif;
+		color: hsl(243, 87%, 12%);
+	}
+</style>
